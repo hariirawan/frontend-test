2b2c9f0471b0831498d78ce5b2a4febe65426c83
hariirawan <hari31121993@gmail.com>
2023-10-10 01:14:15 +0800

    setup jest adn React Testing Library

---
+++ jest.config.mjs
@@ -0,0 +1,13 @@
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: "jest-environment-jsdom",
+};
+
+export default createJestConfig(config);
