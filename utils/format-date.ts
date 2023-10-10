cc8dabcbdb6b85c8a2a42f983545c631b0b8cd77
hariirawan <hari31121993@gmail.com>
2023-10-10 08:39:42 +0800

    add format-date

---
+++ utils/format-date.ts
@@ -0,0 +1,10 @@
+export const formatDate =(date:string)=> {
+    const originalDate = new Date(date);
+    const year = originalDate.getUTCFullYear();
+    const month = String(originalDate.getUTCMonth() + 1).padStart(2, "0");
+    const day = String(originalDate.getUTCDate()).padStart(2, "0");
+    
+    const formattedDate = `${year}-${month}-${day}`;
+    console.log(formattedDate)
+    return formattedDate;
+}
